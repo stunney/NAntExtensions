a6b28c57e002869e42bad10221fe874140356ee0
stunney <s_tunney2000@yahoo.com>
2014-03-13 11:42:02 -0400

    Updating licensing and copyright on all files
    Adding Dave.

--- NAnt.Extensions/Tasks/WiX/Heat.cs
+++ NAnt.Extensions/Tasks/WiX/Heat.cs
@@ -1,7 +1,7 @@
 ﻿/*
 The MIT License (MIT)
 
-Copyright (c) 2013-2014 Stephen Tunney, Canada (stephen.tunney@gmail.com)
+Copyright (c) 2013-2014 Dave Tuchlinsky, Stephen Tunney, Canada (stephen.tunney@gmail.com)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
